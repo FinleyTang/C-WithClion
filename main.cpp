04dc6d3263d4ef0cc6729f3b8f3b1c0383f31f34
FinleyTang <finleyt@163.com>
2023-10-29 21:45:53 +0800

    my first time using clion writing C++ code

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int sum(int a, int b){
+    return  a+b;
+
+
+}
+
+int main() {
+    int a = 3;
+    int b = 4;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << sum(a, b)<<std::endl;
+    return 0;
+}
