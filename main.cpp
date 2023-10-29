e9cd97d5fa65dcdb2e2daf2e05be9ab5ed3927ed
FinleyTang <finleyt@163.com>
2023-10-29 21:55:27 +0800

    simple class test

--- main.cpp
+++ main.cpp
@@ -3,13 +3,29 @@
 int sum(int a, int b){
     return  a+b;
 
-
 }
+
+class TestClass{
+private:
+    int id;
+public:
+    //
+    int getId() const {
+        return id;
+    }
+
+    void setId(int id) {
+        TestClass::id = id;
+    }
+};
 
 int main() {
     int a = 3;
     int b = 4;
     std::cout << "Hello, World!" << std::endl;
     std::cout << sum(a, b)<<std::endl;
+    TestClass test1{};
+    test1.setId(4);
+    std::cout << test1.getId()<<std::endl;
     return 0;
 }
